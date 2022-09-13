6dc80277c41adec2ef0db0ecf88bb4ac067e5a19
Eduardo Lima <mendes.eduardolima@gmail.com>
2022-09-12 22:01:42 -0300

    criação dos componentes

---
+++ src/app/alo-mundo/alo-mundo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alo-mundo',
+  templateUrl: './alo-mundo.component.html',
+  styleUrls: ['./alo-mundo.component.css']
+})
+export class AloMundoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AloMundoComponent } from './alo-mundo/alo-mundo.component';
+import { ComponenteTesteComponent } from './componente-teste/componente-teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AloMundoComponent,
+    ComponenteTesteComponent
   ],
   imports: [
     BrowserModule,
